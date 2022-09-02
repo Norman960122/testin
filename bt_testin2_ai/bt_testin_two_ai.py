1638aab0180edf147928d0fa6a7da94ee61fd3cf
Norman Chen <normchen960122@gmail.com>
2022-09-02 16:44:50 +0800

    加rfid和改懲罰判斷式

--- bt_testin2_ai/bt_testin_two_ai.py
+++ bt_testin2_ai/bt_testin_two_ai.py
@@ -4,12 +4,13 @@ import cv2
 from time import sleep
 from time import time
 import numpy as np
+import copy
 from drive_func2 import car_control
 from drive_func2 import get_new_text
 from drive_func2 import add_horn_turnsig
 from video_func2 import record
 from tensorflow import keras
-from tensorflow.keras.models import load_model#import 要這樣打才可以跑   hello3
+from tensorflow.keras.models import load_model#import 要這樣打才可以跑
 #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 def change_str_float_array(feedback):
     a = feedback.split(',')
@@ -19,7 +20,7 @@ def change_str_float_array(feedback):
     return b
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 model_long = load_model('model97.h5')#--------------------------載入模型
-model_short = load_model('model_short95.h5')#--------------------------載入模型
+model_short = load_model('model_short933.h5')#--------------------------載入模型
 file_name = input("enter file name to write into:")
 COM_number = input("enter com port number:")
 cam_number = input("enter cam number:")
@@ -37,12 +38,15 @@ left_sig_text = ''
 right_sig_text = ''
 moving_direction_text = ''
 horn_text = ''
+horn_num = 0
+max_horn_num = 9
 record_text = ''
 file_type = '.txt'
 pred_text_long = ''
 pred_text_short = ''
 prev_pred_text_short = ''
 prev_pred_text_long = ''
+rfid_text = 'car unlocked'
 preds_long = 0
 run = True
 save_flag = False #---------------------------------------------------------------是否開始存陣列
@@ -54,10 +58,12 @@ onetime_short = 0
 arr_save_counter = 0 #-----------------------------------------------------------儲存陣列用的計數器
 arr_save_counter_short = 0 #-----------------------------------------------------------儲存陣列用的計數器
 times_to_save = 20 #---------------------------------------------------------------一次要儲存幾個陣列
-punish_countdown = 3
+punish_countdown = 10
 
 danger_flag = 0
+danger_flag_long = 0
 danger_flag_time = 0
+danger_flag_long_time = 0
 write_flag = 0  #判斷是否為寫入模式
 out = 0
 cap = cv2.VideoCapture(int(cam_number))
@@ -67,7 +73,7 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, _CAMERA_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, _CAMERA_HEIGH)
 # 設定擷取影像的尺寸大小
 
-
+ser_rfid = serial.Serial('COM8', 9600)#----------------------------------rfid的鏈接
 ser = serial.Serial(COM_PORT, BAUD_RATES, timeout = 0.01)
 pygame.init()#---------------------------------------------------------------pygame初始化
 winScreen = pygame.display.set_mode((S_W, S_H))
@@ -127,7 +133,13 @@ while run:
                 arr_save_counter = 0
 
             # print(float_array_2d)
-            sample_long = float_array_2d.reshape(1, 10, 4)
+            horn_num = 0
+            for i in range(array_hight):###################計算一列有多少喇叭
+                horn_num = horn_num + int(float_array_2d[i, 1])
+            print(horn_num)
+            sample_long = copy.deepcopy(float_array_2d.reshape(1, 10, 4))
+            sample_long[:, :, 1] = 0 #change horn
+
             preds_long = model_long.predict_classes(sample_long)#判斷資料(lstm)
             if preds_long + 1 == 8:
                 if keys[pygame.K_w]:
@@ -145,7 +157,8 @@ while run:
                 elif keys[pygame.K_s]:
                     pred_text_long = 'backward right signal snaking'
             else :
-                pred_text_long = ''
+                pred_text_long = 'no snaking'
+            # print(pred_text_long)
             float_array_2d = np.delete(float_array_2d, (0), axis = 0)#-----------刪除第一列
             # print('\n')
         #-----------------------------------------------------------------------------------------------------------------（以下為短矩陣儲存部分）（以上為長矩陣儲存部分）
@@ -165,7 +178,11 @@ while run:
                 save_flag_short = False
                 arr_save_counter_short = 0
 
-            sample_short = float_array_2d_short.reshape(1, 5, 4)
+            print(float_array_2d_short)
+
+            
+            sample_short = copy.deepcopy(float_array_2d_short.reshape(1, 5, 4))
+            sample_short[:,:, 1] = 0#change horn
             preds_short = model_short.predict_classes(sample_short)#判斷資料(lstm)
             if preds_short + 1 == 1:
                 pred_text_short = 'normal'
@@ -221,30 +238,51 @@ while run:
                     pred_text_short = 'forward ' + pred_text_short
                 elif keys[pygame.K_s]:
                     pred_text_short = 'backward ' + pred_text_short   
-            if (not(('normal' in pred_text_short)or('left signal left' in pred_text_short)or('right signal right' in pred_text_short)or('right_signal' in pred_text_short)or('left_signal' in pred_text_short))    or  (preds_long + 1 == 8)or(preds_long + 1 == 11)or(preds_long + 1 == 12)):
-                if (danger_flag == 0) or(not(prev_pred_text_short == pred_text_short)):#三秒後 或 危險形態有變才扣分
+            if (not(('normal' in pred_text_short)or('left signal left' in pred_text_short)or('right signal right' in pred_text_short)or('right_signal' in pred_text_short)or('left_signal' in pred_text_short))):
+                if (danger_flag == 0) or(not(prev_pred_text_short == pred_text_short)):#1.5秒後 或 危險形態有變才扣分
                     punish_countdown = punish_countdown - 1
                     danger_flag = 1
                     danger_flag_time = pygame.time.get_ticks()#---------------------取得偵測到危險駕駛的時間
-                if punish_countdown == 0:
-                    print('punishingjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
-                    ser.write(b'p\n')
-                    rfid_text = 'car slowed'
-                    punish_countdown = 3
+
+            if  (preds_long + 1 == 8)or(preds_long + 1 == 11)or(preds_long + 1 == 12):
+                if (danger_flag_long == 0) or(not(prev_preds_long == preds_long)):#3秒後 或 危險形態有變才扣分
+                    punish_countdown = punish_countdown - 1
+                    danger_flag_long = 1
+                    danger_flag_long_time = pygame.time.get_ticks()#---------------------取得偵測到危險駕駛的時間
+            if (horn_num > max_horn_num):
+                punish_countdown = punish_countdown - 1
+
+            if punish_countdown <= 0:
+                print('punishingjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
+                ser.write(b'p\n')
+                rfid_text = 'car slowed'
+                punish_countdown = 10
                 
-                prev_pred_text_short = pred_text_short
-                print(prev_pred_text_short)
+            prev_pred_text_short = pred_text_short
+            prev_preds_long = preds_long
+            # print(prev_pred_text_short)
             print(punish_countdown) 
     
-            print(float_array_2d_short)
+            
             float_array_2d_short = np.delete(float_array_2d_short, (0), axis = 0)#-----------刪除第一列
             print('\n')
 
-    
+    while ser_rfid.in_waiting:#----------------get rifd input
+        mcu_feedback2 = ser_rfid.readline().decode() 
+        if mcu_feedback2 == "one\n":
+            ser.write(b'one\n')
+            rfid_text = 'car locked'
+        elif mcu_feedback2 == "two\n":
+            ser.write(b'two\n')
+            rfid_text = 'car unlocked'
+            punish_countdown = 10
+        print(mcu_feedback2)
     
     current_time = pygame.time.get_ticks()#---------------------------------------------取得現在時間
     if current_time - danger_flag_time > 1500:#---------------------------偵測到危險駕駛緩衝時間三秒
         danger_flag = 0
+    if current_time - danger_flag_long_time > 3000:
+        danger_flag_long = 0
         
 
 
@@ -261,6 +299,8 @@ while run:
     record_text_surface =  my_font.render(record_text, True, (0, 0, 0))
     prediction_text_surface_long = my_font.render(pred_text_long, True, (0, 0, 0))
     prediction_text_surface_short = my_font.render(pred_text_short, True, (0, 0, 0))
+    rfid_text_surface = my_font.render(rfid_text, True, (0, 0, 0))
+    points_text =  my_font.render(str(punish_countdown), True, red)
 
     move_text = my_font.render('moving direction(press w a s d):', True, red)
     signal_text = my_font.render('turn signal(press < >):', True, blue)
@@ -268,6 +308,8 @@ while run:
     record_state_text = my_font.render('record(press f ):', True, (0, 0, 0))
     pred_state_text = my_font.render('lstm predicton(long term)', True, (0, 0, 0))
     pred_state_text2 = my_font.render('lstm predicton(short term)', True, (0, 0, 0))
+    rfid_state_text = my_font.render('car state:', True, (0, 0, 0))
+    point_state_text = my_font.render('points left:', True, red)
 
     winScreen.fill([255,255,255])
     winScreen.blit(drive_text_surface, (500, 10))#------------------------------------印出文字
@@ -277,12 +319,17 @@ while run:
     winScreen.blit(record_text_surface, (500, 90))
     winScreen.blit(prediction_text_surface_long, (500, 110))
     winScreen.blit(prediction_text_surface_short, (500, 130))
+    winScreen.blit(rfid_text_surface, (500, 150))
+    winScreen.blit(points_text,(500, 170))
+
     winScreen.blit(move_text, (10, 10))
     winScreen.blit(signal_text, (10, 30))
     winScreen.blit(horn_state_text, (10, 70))
     winScreen.blit(record_state_text, (10, 90))
     winScreen.blit(pred_state_text, (10, 110))
     winScreen.blit(pred_state_text2, (10, 130))
+    winScreen.blit(rfid_state_text, (10, 150))
+    winScreen.blit(point_state_text, (10, 170))
     pygame.display.update()
     # time_end = time() 
     # print(time_end - time_start, 's')
