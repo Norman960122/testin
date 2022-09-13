287ebde66870ea76cd37879acf2a91cc91b7ab9b
Norman Chen <normchen960122@gmail.com>
2022-09-13 15:35:43 +0800

    新增xgboost 模型

--- train/testin_train_adaboost/testin_train_adaboost.py
+++ train/testin_train_adaboost/testin_train_adaboost.py
@@ -10,26 +10,27 @@ x_test = np.loadtxt('x_test_short3.txt')
 y_test = np.loadtxt('y_test_short3.txt')
 x_train = x_train.reshape(-1, 20)
 x_test = x_test.reshape(-1, 20)
-# x_train = np.asmatrix(x_train)
-# x_test = np.asmatrix(x_test)
-# y_train = list(y_train)
-# y_test = list(y_test)
-bdt_real = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=2), n_estimators=400, learning_rate=1
-)
 
-bdt_discrete = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=2),
-    n_estimators=400,
-    learning_rate=1.5,
-    algorithm="SAMME",
-)
+for i in range(450, 451, 50):
+    bdt_real = AdaBoostClassifier(
+        DecisionTreeClassifier(max_depth=2), n_estimators=i, learning_rate=1.5
+    )
 
-bdt_real.fit(x_train, y_train)
-bdt_discrete.fit(x_train, y_train)
-print('aaaaaaaaaaaaaaaaaaaaaaa')
-real_test_predict, discrete_test_predict = bdt_real.predict(x_test), bdt_discrete.predict(x_test)
-print('bbbbbbbbbbbbbbbbbbbbb')
-print( accuracy_score(real_test_predict, y_test))
-print(accuracy_score(discrete_test_predict, y_test))
+    bdt_discrete = AdaBoostClassifier(
+        DecisionTreeClassifier(max_depth=2),
+        n_estimators=i,
+        learning_rate=1.5,
+        algorithm="SAMME",
+    )
+
+    bdt_real.fit(x_train, y_train)
+    bdt_discrete.fit(x_train, y_train)
+    real_train_predict, discrete_train_predict = bdt_real.predict(x_train), bdt_discrete.predict(x_train)
+    real_test_predict, discrete_test_predict = bdt_real.predict(x_test), bdt_discrete.predict(x_test)
+    print('n_estimators = %d' %i)
+    print('SAMME.R training set accuracy:', accuracy_score(real_train_predict, y_train))
+    print('SAMME.R testing set accuracy:', accuracy_score(real_test_predict, y_test))
+    print('SAMME training set accuracy:', accuracy_score(discrete_train_predict, y_train))
+    print('SAMME testing set accuracy:', accuracy_score(discrete_test_predict, y_test))
+    print('-----------------------------------------------------------------------------------------------')
 # print(len(list(real_test_predict)))
---
+++ train/testin_train_xgboost/testin_train_xgboost.py
@@ -0,0 +1,71 @@
+import numpy as np
+import xgboost as xgb
+
+from sklearn import datasets
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import LabelEncoder 
+
+from xgboost import XGBClassifier
+
+from sklearn.metrics import accuracy_score
+
+x_train = np.loadtxt('x_train_short2.txt')
+y_train = np.loadtxt('y_train_short2.txt')
+x_test = np.loadtxt('x_test_short3.txt')
+y_test = np.loadtxt('y_test_short3.txt')
+x_train = x_train.reshape(-1, 20)
+x_test = x_test.reshape(-1, 20)
+y_test = y_test - 1
+y_train = y_train - 1
+print(x_train.shape, y_train.shape)
+model = XGBClassifier() 
+
+model.fit(x_train, y_train)
+y_pred_train = model.predict(x_train) 
+y_pred_test = model.predict(x_test) 
+
+
+train_accuracy = accuracy_score(y_train, y_pred_train) 
+test_accuracy = accuracy_score(y_test, y_pred_test) 
+print("train Accuracy: %f%%" % (train_accuracy * 100.0))
+print("test Accuracy: %f%%" % (test_accuracy * 100.0))
+# import xgboost as xgb
+
+# from sklearn import datasets
+
+# from sklearn.model_selection import train_test_split
+
+# from sklearn.preprocessing import LabelEncoder 
+
+# from xgboost import XGBClassifier
+
+# from sklearn.metrics import accuracy_score
+
+# iris = datasets.load_iris()
+
+# X = iris.data
+
+# y = iris.target
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=5)
+
+# lc = LabelEncoder() 
+
+# lc = lc.fit(y) 
+
+# lc_y = lc.transform(y)
+
+# model = XGBClassifier() 
+
+# model.fit(X_train, y_train)
+
+# y_pred = model.predict(X_test) 
+
+# predictions = [round(value) for value in y_pred]
+
+# accuracy = accuracy_score(y_test, predictions) 
+
+# print("Accuracy: %.2f%%" % (accuracy * 100.0))
+# print(X_train.shape, y_train.shape)
